456aec071474367d6c85d23f6ab341ba3c035b7f
Kn0wn-Un <naikdarsh175@gmail.com>
2020-11-24 23:56:03 +0530

    online hosting

--- client/src/Name.js
+++ client/src/Name.js
@@ -2,17 +2,32 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function Name() {
-    const [name, setName] = useState('');
+    const [name, setName] = useState([]);
+    const [gotData, setGot] = useState(false);
+    const mkArr = () => {
+        const nameArr = name.map((row) => {
+            return <li key={row.id}>{row.name}</li>;
+        });
+        return nameArr;
+    };
     useEffect(() => {
         console.log('Hello World from Name');
         fetch('/shop')
             .then((res) => res.json())
-            .then((data) => setName(data.name));
+            .then((data) => {
+                setName(data);
+                setGot(true);
+            });
         return () => {
             console.log('Unmounted Name');
         };
     }, []);
-    return <div className="App">Name: {name}</div>;
+    useEffect(() => {
+        console.log(name);
+    }, [gotData]);
+    return (
+        <div className="App">{gotData ? <ol>{mkArr()}</ol> : 'loading'} </div>
+    );
 }
 
 export default Name;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+// get the client
+const mysql = require('mysql2');
 
 const app = express();
 
@@ -7,9 +9,22 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/shop', (req, res) => {
     const name = 'Something';
-    res.json({ name });
 
-    console.log(`Sent name`);
+    // create the connection to database
+    const connection = mysql.createConnection({
+        host: 'remotemysql.com',
+        user: 'REXhJJZHIB',
+        password: 'lScmGPJoo5',
+        database: 'REXhJJZHIB',
+    });
+    connection.query('SELECT * FROM `test`', function (err, results) {
+        if (err) throw err;
+        const arr = results.map((table) => {
+            return { id: table.id, name: table.name };
+        });
+        //console.log(arr);
+        res.json(arr);
+    });
 });
 
 app.get('*', (req, res) => {
