1328d87a4997d80e10f2f0075d1a1b27e9328e0f
Kn0wn-Un <naikdarsh175@gmail.com>
2020-12-17 16:16:17 +0530

    movie trailer and watch providers

--- client/src/MovieDetail.js
+++ client/src/MovieDetail.js
@@ -7,11 +7,66 @@ function MovieDetail({ match, isAuth, user }) {
     const [movie, setmovie] = useState({});
     const [gotData, setGot] = useState(false);
     const [poster, setPoster] = useState('');
-    const getPoster = async (id) => {
-        await fetch(`http://www.omdbapi.com/?i=${id}&apikey=7b26edf7`)
+    const [trailer, setTrailer] = useState('');
+    const [watch, setWatch] = useState(false);
+    const [rent, setRent] = useState([]);
+    const [ott, setOtt] = useState([]);
+    const [buy, setBuy] = useState([]);
+    const getPoster = async (name) => {
+        await fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=7f082a6e3dcc6c228b449d18649a5f25&query=${name}&page=1`
+        )
             .then((res) => res.json())
             .then((data) => {
-                setPoster(data.Poster);
+                setPoster(
+                    'https://image.tmdb.org/t/p/original/' +
+                        data.results[0].poster_path
+                );
+                console.log(data);
+                return fetch(
+                    `https://api.themoviedb.org/3/movie/${data.results[0].id}/videos?api_key=7f082a6e3dcc6c228b449d18649a5f25`
+                );
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                for (let i = 0; i < data.results.length; i++) {
+                    if (data.results[i].type === 'Trailer')
+                        setTrailer(data.results[i].key);
+                }
+                console.log(data);
+                return fetch(
+                    `https://api.themoviedb.org/3/movie/${data.id}/watch/providers?api_key=7f082a6e3dcc6c228b449d18649a5f25`
+                );
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data.results.IN);
+                console.log(data.results.IN.flatrate);
+                let lData = data.results.IN;
+                if (lData !== undefined) {
+                    setWatch(true);
+                    setBuy(
+                        lData.buy.map((provider, index) => {
+                            return (
+                                <li key={index}>{provider.provider_name}</li>
+                            );
+                        })
+                    );
+                    setRent(
+                        lData.rent.map((provider, index) => {
+                            return (
+                                <li key={index}>{provider.provider_name}</li>
+                            );
+                        })
+                    );
+                    setOtt(
+                        lData.flatrate.map((provider, index) => {
+                            return (
+                                <li key={index}>{provider.provider_name}</li>
+                            );
+                        })
+                    );
+                }
             })
             .catch(() => {});
     };
@@ -61,15 +116,40 @@ function MovieDetail({ match, isAuth, user }) {
                             <span className="heading">Summary:</span>{' '}
                             {movie.description}
                         </div>
-                        <div className="trailer">
-                            <h1>Trailer</h1>
-                            <iframe
-                                title={movie.name}
-                                width="420"
-                                height="315"
-                                src="https://www.youtube.com/embed/9GVMoxhtrY8"
-                            ></iframe>{' '}
-                        </div>
+                        {trailer !== '' ? (
+                            <div className="trailer">
+                                <h1>Trailer</h1>
+                                <iframe
+                                    title={movie.name}
+                                    width="420"
+                                    height="315"
+                                    src={`https://www.youtube.com/embed/${trailer}`}
+                                ></iframe>{' '}
+                            </div>
+                        ) : null}
+                        {watch ? (
+                            <div className="watch">
+                                <h1>Where to Watch:</h1>
+                                {buy.length > 0 ? (
+                                    <div>
+                                        <h4 className="heading">Buy:</h4>
+                                        <ul>{buy}</ul>
+                                    </div>
+                                ) : null}
+                                {rent.length > 0 ? (
+                                    <div>
+                                        <h4 className="heading">Rent:</h4>
+                                        <ul>{rent}</ul>
+                                    </div>
+                                ) : null}
+                                {ott.length > 0 ? (
+                                    <div>
+                                        <h4 className="heading">OTT:</h4>
+                                        <ul>{ott}</ul>
+                                    </div>
+                                ) : null}
+                            </div>
+                        ) : null}
                     </div>
                 </div>
             </div>
@@ -81,10 +161,11 @@ function MovieDetail({ match, isAuth, user }) {
             .then((data) => {
                 setmovie(data);
                 setGot(true);
+                getPoster(data.name.replace(' ', '%20'));
             });
     }, []);
     useEffect(() => {
-        getPoster(movie.imdb_id);
+        //getPoster(movie.name.replace(' ', '%20'));
     }, [gotData]);
     return (
         <div className="App">
